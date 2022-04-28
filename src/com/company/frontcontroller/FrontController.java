04638f299ac8de4edae0139017e8f97b48ce5e75
JPQueirozPerez <jpqueirozperez@gmail.com>
2022-04-28 12:44:45 +0200

    Commit #test:

---
+++ src/com/company/frontcontroller/FrontController.java
@@ -0,0 +1,4 @@
+package com.company.frontcontroller;
+
+public class FrontController {
+}
---
+++ src/com/company/view/IOView.java
@@ -0,0 +1,7 @@
+package com.company.view;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class IOView {
+    private int prueba;
+}
