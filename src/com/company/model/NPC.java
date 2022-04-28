399c5fe60817f5cc04d4ae7a32fd13c71bb4dbec
fabiarza <fabianaarzamendia10@gmail.com>
2022-04-28 13:44:47 +0200

    commit #chore:First commit model.

---
+++ src/com/company/model/NPC.java
@@ -0,0 +1,10 @@
+package com.company.model;
+import com.company.model.Item;
+
+public class NPC {
+ private   Item treasure;
+
+    public NPC(Item treasure) {
+        this.treasure = treasure;
+    }
+}
---
+++ src/com/company/model/Player.java
@@ -0,0 +1,10 @@
+package com.company.model;
+import com.company.model.Inventory;
+
+public class Player {
+  private   Inventory inventory;
+
+    public Player(Inventory inventory) {
+        this.inventory = inventory;
+    }
+}
