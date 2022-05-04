13599a50dd0fb14d7add320ab36a692c33de0b11
JPQueirozPerez <jpqueirozperez@gmail.com>
2022-05-04 14:01:35 +0200

    Commit #refactor: refactor FightService

--- src/com/company/controller/FightController.java
+++ src/com/company/controller/FightController.java
@@ -8,6 +8,7 @@ import java.util.Random;
 import static com.company.controller.CharacterController.createEnemyPlaceholder;
 import static com.company.controller.CharacterController.createPlayer;
 import static com.company.controller.InventoryController.addItemToInventory;
+import static com.company.service.FightService.fightDevelopment;
 import static com.company.service.FightService.fightResult;
 import static com.company.view.IOView.optionsMainMenu;
 //import static com.company.service.FightService.fightDevolving;
@@ -25,13 +26,7 @@ public class FightController {
     public static Player fighting(Player player) {
         NPC enemy = createEnemyPlaceholder();
         System.out.println("Your enemy is a "+enemy.getName());
-        int playerLevel = player.getLevel();
-        int enemyHealthPoints = 30;
-        int enemyStrength = enemy.getStrength();
-        int enemyDefense = (new Random().nextInt(player.getDefense() - (player.getDefense()-5)))+1;
-        //int enemySpeed = (new Random().nextInt((10 - 5) + 1))+1;
-
-        fightResult(enemy, player, playerLevel);
+        fightDevelopment(enemy, player);
         return player;
     }
 }
--- src/com/company/service/FightService.java
+++ src/com/company/service/FightService.java
@@ -12,28 +12,85 @@ import static com.company.view.IOView.optionsMainMenu;
 
 public class FightService {
 
-    public static void fightResult(NPC enemy, Player player, int playerLevel) {
-
-        int enemyDamage = (enemy.getStrength() * (enemy.getStrength() / player.getDefense()))+5;
-
-        int playerDamage = (player.getStrength() * (player.getStrength() / enemy.getDefense()))+5;
+    public static void fightDevelopment(NPC enemy, Player player){
         if (player.getSpeed() <= enemy.getSpeed()) {
-            player.setHealthPoints(player.getHealthPoints() - enemyDamage);
-            System.out.println("The enemy makes you " + enemyDamage + " points of damage!");
-            System.out.println(player.getHealthPoints());
-            enemy.setHealthPoints(enemy.getHealthPoints() - playerDamage);
-            System.out.println("You makes " + playerDamage + " points of damage to your enemy!");
-            System.out.println(enemy.getHealthPoints());
+            for(int i = 0; i<10; i++){
+                if(attackSuccess(enemy, player)){
+                    System.out.println("Your enemy successes their attack!");
+                    player.setHealthPoints(player.getHealthPoints() - fightDamage(enemy, player));
+                    System.out.println("The enemy makes you " + fightDamage(enemy, player) + " points of damage!");
+                    System.out.println(player.getHealthPoints());
+                    if(player.getHealthPoints()<=0) fightResult(enemy,player);
+                    else{
+                        enemy.setHealthPoints(enemy.getHealthPoints() - fightDamage(player, enemy));
+                        System.out.println("You makes " + fightDamage(player, enemy) + " points of damage to your enemy!");
+                        System.out.println(enemy.getHealthPoints());
+                        fightResult(enemy,player);
+                    }
+                    break;
+                }else{
+                    for(int j = 0; j<10; j++){
+                        if(attackSuccess(player, enemy)){
+                            System.out.println("You successes your attack!");
+                            enemy.setHealthPoints(enemy.getHealthPoints() - fightDamage(player, enemy));
+                            System.out.println("You makes " + fightDamage(player, enemy) + " points of damage to your enemy!");
+                            System.out.println(enemy.getHealthPoints());
+                            if(enemy.getHealthPoints()<=0) fightResult(enemy,player);
+                            else{
+                                player.setHealthPoints(player.getHealthPoints() -  fightDamage(enemy, player));
+                                System.out.println("The enemy makes you " + fightDamage(enemy, player) + " points of damage!");
+                                System.out.println(player.getHealthPoints());
+                                fightResult(enemy,player);
+                            }
+                            System.out.println("You have failed your attack!");
+                            break;
+                        }
+                    }
+                }
+                System.out.println("Your enemy have failed their attack!");
+                break;
+            }
         }else{
-            enemy.setHealthPoints(enemy.getHealthPoints() - playerDamage);
-            System.out.println("You makes " + playerDamage + " points of damage to your enemy!");
-            System.out.println(enemy.getHealthPoints());
-            player.setHealthPoints(player.getHealthPoints() - enemyDamage);
-            System.out.println("The enemy makes you " + enemyDamage + " points of damage!");
-            System.out.println(player.getHealthPoints());
+            for(int i = 0; i<10; i++){
+                if(attackSuccess(player, enemy)){
+                    System.out.println("You successes your attack!");
+                    enemy.setHealthPoints(enemy.getHealthPoints() - fightDamage(player, enemy));
+                    System.out.println("You makes " + fightDamage(player, enemy) + " points of damage to your enemy!");
+                    System.out.println(enemy.getHealthPoints());
+                    if(enemy.getHealthPoints()<=0) fightResult(enemy,player);
+                    else{
+                        player.setHealthPoints(player.getHealthPoints() -  fightDamage(enemy, player));
+                        System.out.println("The enemy makes you " + fightDamage(enemy, player) + " points of damage!");
+                        System.out.println(player.getHealthPoints());
+                        fightResult(enemy,player);
+                    }
+                    break;
+                }else{
+                    for(int j = 0; j<10; j++){
+                        if(attackSuccess(enemy, player)){
+                            System.out.println("Your enemy successes their attack!");
+                            player.setHealthPoints(player.getHealthPoints() - fightDamage(enemy, player));
+                            System.out.println("The enemy makes you " + fightDamage(enemy, player) + " points of damage!");
+                            System.out.println(player.getHealthPoints());
+                            if(player.getHealthPoints()<=0) fightResult(enemy,player);
+                            else{
+                                enemy.setHealthPoints(enemy.getHealthPoints() - fightDamage(player, enemy));
+                                System.out.println("You makes " + fightDamage(player, enemy) + " points of damage to your enemy!");
+                                System.out.println(enemy.getHealthPoints());
+                                fightResult(enemy,player);
+                            }
+                            System.out.println("Your enemy have failed their attack!");
+                            break;
+                        }
+                    }
+                }
+                System.out.println("You have failed your attack!");
+                break;
+            }
         }
+    }
 
-
+    public static void fightResult(NPC enemy, Player player) {
         if (player.getHealthPoints() <= 0) optionsMainMenu(createPlayer());
         else if (enemy.getHealthPoints() <= 0) {
             player.setLevel(player.getLevel()+1);
@@ -47,124 +104,21 @@ public class FightService {
             player.setInventory(addItemToInventory(player.getInventory().getItems(), player.getInventory()));
         } else{
             System.out.println("You need to attack again");
-            fightResult(enemy, player, playerLevel);
+            fightDevelopment(enemy, player);
         }
     }
 
-//    public static int enemyLevel(Character player) {
-//        int playerLevel = player.getLevel();
-//        int enemyLevel;
-//
-//        if (playerLevel <= 5) {
-//            enemyLevel = playerLevel;
-//        } else {
-//            enemyLevel = new Random().nextInt(((playerLevel + 1) - (playerLevel - 1)) + 1) + playerLevel;
-//        }
-//        return enemyLevel;
-//    }
-//
-//    public static void fightDevolving(NPC enemy, Player player, int playerLevel) {
-//        if (player.getSpeed() <= enemy.getSpeed()) {
-////            for (int i = 0; i < 10; i++) {
-////                if (attackSuccessLoop(enemy, player, player, enemy, playerLevel) == true) {
-////                    damageVerification(enemy, player, player, enemy, playerLevel);
-////                } else {
-////                    if (attackSuccessLoop(player, enemy, player, enemy, playerLevel) == true) {
-////                        damageVerification(player, enemy, player, enemy, playerLevel);
-////                    }
-////                }
-////            }
-//            damageVerification(enemy, player, player, enemy, playerLevel);
-//
-////            if(attackSuccessLoop(enemy, player,  player, enemy, playerLevel) == true){
-////                damageVerification(enemy, player, player, enemy, playerLevel);
-////            }else{
-////                if(attackSuccessLoop(player, enemy,  player, enemy, playerLevel) == true){
-////                    damageVerification(player, enemy, player, enemy, playerLevel);
-////                }else{
-////                    System.out.println("The 2 had failed!");
-////                }
-////            }
-//        } else {
-////            for (int i = 0; i < 10; i++) {
-////                if (attackSuccessLoop(player, enemy, player, enemy, playerLevel) == true) {
-////                    damageVerification(player, enemy, player, enemy, playerLevel);
-////                } else {
-////                    if (attackSuccessLoop(enemy, player, player, enemy, playerLevel) == true) {
-////                        damageVerification(enemy, player, player, enemy, playerLevel);
-////                    }
-////                }
-////            }
-//            damageVerification(player, enemy, player, enemy, playerLevel);
-//        }
-////            if (attackSuccess(player, enemy)) {
-////                player.setHealthPoints(player.getHealthPoints() - fightDamage(enemy, player));
-////                System.out.println("The enemy makes you " + fightDamage(enemy, player) + " points of damage!");
-////                System.out.println(player.getHealthPoints());
-////                if (player.getHealthPoints() <= 0) fightResult(enemy, player, playerLevel);
-////                else {
-////                    if (attackSuccess(enemy, player)) {
-////                        enemy.setHealthPoints(enemy.getHealthPoints() - fightDamage(player, enemy));
-////                        System.out.println("You makes " + fightDamage(player, enemy) + " points of damage to your enemy!");
-////                        System.out.println(enemy.getHealthPoints());
-////                        fightResult(enemy, player, playerLevel);
-////                    }else{
-////                        System.out.println("Your enemy missed their attack!");
-////                        fightDamage(player, enemy);
-////                    }
-////                }
-////            }
-////            else{
-////                System.out.println("You missed your attack!");
-////                fightDamage(enemy, player);
-////            }
-////        } else {
-////            if (attackSuccess(enemy, player)) {
-////                enemy.setHealthPoints(enemy.getHealthPoints() - fightDamage(player, enemy));
-////                System.out.println("You makes " + fightDamage(player, enemy) + " points of damage to your enemy!");
-////                System.out.println(enemy.getHealthPoints());
-////                if (enemy.getHealthPoints() <= 0) fightResult(enemy, player, playerLevel);
-////                else {
-////                    if (attackSuccess(player, enemy)) {
-////                        player.setHealthPoints(player.getHealthPoints() - fightDamage(enemy, player));
-////                        System.out.println("The enemy makes you " + fightDamage(enemy, player) + " points of damage!");
-////                        System.out.println(player.getHealthPoints());
-////                        fightResult(enemy, player, playerLevel);
-////                    }else{
-////                        System.out.println("You missed your attack!");
-////                        fightDamage(enemy, player);
-////                    }
-////                }
-////            }else{
-////                System.out.println("Your enemy missed their attack!");
-////                fightDamage(player, enemy);
-////            }
-////        }
-//    }
-//
-//    public static void fightResult(NPC enemy, Player player, int playerLevel) {
-//        if (player.getHealthPoints() <= 0) optionsMainMenu(createPlayer());
-//        else if (enemy.getHealthPoints() <= 0) {
-//            player.setLevel(player.getLevel() + 1);
-//            System.out.println("You have leveled up! Your level is now " + player.getLevel());
-//            player.setHealthPoints(player.getHealthPoints() + 50);
-//            player.setStrength(player.getStrength() + 5);
-//            player.setDefense(player.getDefense() + 5);
-//            player.setSpeed(player.getSpeed() + 5);
-//            System.out.println("You're stronger! Now your strength is " + player.getStrength() + ", your defense is " + player.getDefense() + ", your speed is "
-//                    + player.getSpeed() + " and you have " + player.getHealthPoints() + " HP");
-//            player.setInventory(addItemToInventory(player.getInventory().getItems(), player.getInventory()));
-//        } else {
-//            System.out.println("You need to attack again");
-//            fightDevolving(enemy, player, playerLevel);
-//        }
-//    }
-//
-//    public static boolean attackSuccess(Character attacker, Character defender) {
-//        Random r = new Random();
-//        int result = r.nextInt(10 - 1) + 1;
-//        return ((attacker.getDexterity() - defender.getDexterity()) > result);
-//    }
+    public static boolean attackSuccess(Character attacker, Character defender) {
+        Random r = new Random();
+        int result = r.nextInt(50 - 1) + 1;
+        return ((attacker.getDexterity() - defender.getDexterity()) > result);
+    }
+
+    public static int fightDamage(Character attacker, Character defender){
+        return (attacker.getStrength() * (attacker.getStrength() / defender.getDefense()) + 5);
+    }
+
+
 //
 //    public static boolean attackSuccessLoop(Character attacker, Character defender, Player player, NPC enemy, int playerLevel){
 ////        while (true){
@@ -180,44 +134,5 @@ public class FightService {
 ////            break;
 ////        }
 //    }
-//
-//    public static int fightDamage(Character attacker, Character defender){
-//        return (defender.getHealthPoints() - (attacker.getStrength() * (attacker.getStrength() / defender.getDefense())) + 5);
-//    }
-//
-//    public static void damageVerification(Character attacker, Character defender, Player player, NPC enemy, int playerLevel){
-//        int damage = fightDamage(attacker, defender);
-//        defender.setHealthPoints(defender.getHealthPoints() - fightDamage(attacker, defender));
-//        System.out.println(attacker.getName()+" makes " + damage + " points of damage!");
-//        System.out.println(attacker.getHealthPoints());
-//        fightResult(enemy, player, playerLevel);
-////        if(attacker.equals(player)){
-////            if (attacker.getHealthPoints() <= 0) optionsMainMenu(createPlayer());
-////            else if (defender.getHealthPoints() <= 0) {
-////                player.setLevel(playerLevel++);
-////                System.out.println("You have leveled up! Your level is now " + player.getLevel());
-////                player.setHealthPoints(player.getHealthPoints() + 50);
-////                player.setStrength(player.getStrength() + 5);
-////                player.setDefense(player.getDefense() + 5);
-////                System.out.println("You're stronger! Now your strength is " + player.getStrength() + ", your defense is " + player.getDefense()+" and you have "+player.getHealthPoints()+" HP");
-////                player.setInventory(addItemToInventory(player.getInventory().getItems(), player.getInventory()));
-////            } else{
-////                System.out.println("You need to attack again");
-////                damageVerification( attacker,  defender,  player,  enemy,  playerLevel);
-////            }
-////        }else{
-////            if (enemy.getHealthPoints() <= 0) fightResult(enemy, player, playerLevel);
-////            else {
-////                if (attackSuccess(enemy, player)) {
-////                    enemy.setHealthPoints(enemy.getHealthPoints() - fightDamage(player, enemy));
-////                    System.out.println("You makes " + fightDamage(player, enemy) + " points of damage to your enemy!");
-////                    System.out.println(enemy.getHealthPoints());
-////                    fightResult(enemy, player, playerLevel);
-////                }else{
-////                    System.out.println("Your enemy missed their attack!");
-////                    fightDamage(player, enemy);
-////                }
-////            }
-////        }
-//    }
+
 }
