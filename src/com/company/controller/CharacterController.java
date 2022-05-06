7f9a3d9e2c02ec301b401f9472d1a0e02271ee27
Juan Pablo Queiroz Pérez <51370813+JPQueirozPerez@users.noreply.github.com>
2022-05-06 11:20:08 +0200

    Revert "David v0.2"

--- src/com/company/controller/CharacterController.java
+++ src/com/company/controller/CharacterController.java
@@ -10,18 +10,12 @@ public class CharacterController {
 
     public static Player createPlayer() {
         HashMap<Item, Integer> items = new HashMap<>();
-        HashMap<String,String> valueInitals = new HashMap();
-        valueInitals.put("attack","5");
-        valueInitals.put("defense","3");
-        valueInitals.put("speed","1");
-
         Inventory playerInventory = new Inventory(items, 10);
         Player player = new Player("Player", 1, playerInventory);
-        Item newItem = new Item("Tunic", "cloth", "A simple tunic", 2,valueInitals);
+        Item newItem = new Item("Tunic", "cloth", "A simple tunic", 2);
         items.put(newItem, 1);
         player.getInventory().setItems(items);
 
         return player;
     }
 }
-
--- src/com/company/controller/InventoryController.java
+++ src/com/company/controller/InventoryController.java
@@ -12,19 +12,12 @@ import static com.company.service.InventoryService.createItem;
 public class InventoryController {
 
     public static void showInventory(Player player) {
-        player.getInventory().getItems().forEach((k, v) ->
-                System.out.format("Name: " + k.getName() + " | Quantity: x" + v + "\nDescription: " + k.getDescription()
-                + "\nAttack: "+k.getItemHabilities().get("attack")+" Defense: "+k.getItemHabilities().get("defense")+ " Speed: "+k.getItemHabilities().get("speed")+ "\n----------------------------------------------------\n"));
-//         System.out.println("----------------------------------------------------");
-
+        player.getInventory().getItems().forEach((k, v) -> System.out.format("Name: " + k.getName() + " | Quantity: x" + v + "\nDescription: " + k.getDescription() + "\n"));
 //                "key: %s, value: %d%n", k, v));
-//     System.out.println("Attack"+player.getInventory().getItemHabilities().get("attack"));
     }
 
     public static Inventory addItemToInventory(HashMap<Item, Integer> items, Inventory playerInventory) {
         Item newItem = createItem();
-
-
         if (playerInventory.getCapacity() > 0) {
             if (compareItems(playerInventory.getItems(), newItem)) {
                 items.replace(newItem, items.get(newItem) + 1);
